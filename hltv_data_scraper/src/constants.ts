b6ec3ac983a7738315b8b93b93667c1a8535ad11
ph <chenyi.ph@outlook.com>
2025-12-21 23:42:24 +0800

    fix output file name wrongs

--- hltv_data_scraper/src/constants.ts
+++ hltv_data_scraper/src/constants.ts
@@ -2,8 +2,8 @@ export const INPUT_FILE = "data/prepare_data.json";
 export const INPUT_MODE_FILE = "data/mode_player_list.json";
 export const OUTPUT_DIR = "out";
 export const OUTPUT_FILE = "out/all_players_data.json";
-export const OUTPUT_YLG_FILE = "out/ylg_data.json";
-export const OUTPUT_NORMAL_FILE = "out/normal_data.json";
+export const OUTPUT_YLG_FILE = "out/ylg_players_data.json";
+export const OUTPUT_NORMAL_FILE = "out/normal_players_data.json";
 
 export enum OUTPUT_MODE {
 	YLG = "ylg",
