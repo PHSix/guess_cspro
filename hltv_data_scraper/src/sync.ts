4d2c4dac307a69de03238848a8046a8d9c7c6ee2
PH <57695072+PHSix@users.noreply.github.com>
2025-12-23 13:00:43 +0800

    Clean/prepare data dedup (#2)
    * update mode_player_list.json
    
    * append more players which played on the major and fix sync

--- hltv_data_scraper/src/sync.ts
+++ hltv_data_scraper/src/sync.ts
@@ -7,23 +7,23 @@ import path from "node:path";
  * cp out/*.json ../client/public/
  */
 export async function syncCopy() {
-	return Promise.all([
-		fs.copyFile(
-			INPUT_MODE_FILE,
-			path.resolve(process.cwd(), "../client/public/mode_player_list.json"),
-		),
+  return Promise.all([
+    fs.copyFile(
+      INPUT_MODE_FILE,
+      path.resolve(process.cwd(), "../app/public/mode_player_list.json")
+    ),
 
-		fs.copyFile(
-			OUTPUT_FILE,
-			path.resolve(process.cwd(), "../client/public/all_players_data.json"),
-		),
-	]);
+    fs.copyFile(
+      OUTPUT_FILE,
+      path.resolve(process.cwd(), "../app/public/all_players_data.json")
+    ),
+  ]);
 }
 
 if (process.argv[1] === __filename) {
-	(async () => {
-		info("开始同步数据...");
-		await syncCopy();
-		info("已完成同步数据...");
-	})();
+  (async () => {
+    info("开始同步数据...");
+    await syncCopy();
+    info("已完成同步数据...");
+  })();
 }
